cdf7b55ccf1bc5d8ceb63947ae951749af66795f
marcelo-bonora <marcelo.bonora@bandtec.com.br>
2020-09-03 20:37:59 -0300

    att da dashboard

--- Projeto SIX MIND/Projeto/projeto-site/public/script/rosquinha.js
+++ Projeto SIX MIND/Projeto/projeto-site/public/script/rosquinha.js
@@ -1,4 +1,23 @@
-var roscaChartData = {
+function dados() {
+    roscaChartDataAtm.datasets[0].data = [];
+    for (let cont = 0; cont < 4; cont++) {
+        roscaChartDataAtm.datasets[0].data.push(parseInt(Math.random() * (24 + 1) + 10))
+    }
+
+    window.myDoughnutAtm.update();
+
+}
+function dadosSolo() {
+    roscaChartDataSolo.datasets[0].data = [];
+    for (let cont = 0; cont < 4; cont++) {
+        roscaChartDataSolo.datasets[0].data.push(parseInt(Math.random() * (24 + 1) + 10))
+    }
+
+    window.myDoughnutSolo.update();
+
+}
+//------------------------  gráfico rosca Atimosfera-------------------------------------------------
+var roscaChartDataAtm = {
     datasets: [{
         data: [],
         legend: 'top',
@@ -30,10 +49,11 @@ var roscaChartData = {
 function gerarRosquinha() {
     setInterval(function() {
         var ctx = document.getElementById('rosquinha').getContext('2d');
-        window.myDoughnut = new Chart(ctx, {
+        window.myDoughnutAtm = new Chart(ctx, {
             type: 'doughnut',
-            data: roscaChartData,
+            data: roscaChartDataAtm,
             options: {
+                animation: false,
                 responsive: true,
                 legend: {
                     position: 'left',
@@ -44,23 +64,89 @@ function gerarRosquinha() {
                     display: false,
                 },
                 animation: {
-                    animateScale: true,
-                    animateRotate: true
+                    animateScale: false,
+                    animateRotate: false
                 }
             }
         });
         dados()
-    }, 3200)
+    }, 4000)
 };
 
 gerarRosquinha();
+//------------------------------------ GRAFICO ROSCA SOLO -----------------------------------------
+var roscaChartDataSolo = {
+    datasets: [{
+        data: [],
+        legend: 'top',
+        backgroundColor: [
+            '#FF1700',
+            '#FFF200',
+            '#FF6F00',
+            '#00FF6F',
+            // '#2849a7',
+        ],
+        label: [
+            'Ideal',
+            'Alerta',
+            'Emergencial',
+            'Crítico',
+            // 'Blue', 
+        ]
+    }],
+    labels: [
+        'Ideal',
+        'Alerta',
+        'Emergencial',
+        'Crítico',
+        // 'Blue'
+    ]
 
-function dados() {
-    roscaChartData.datasets[0].data = [];
-    for (let cont = 0; cont < 4; cont++) {
-        roscaChartData.datasets[0].data.push(parseInt(Math.random() * (24 + 1) + 10))
-    }
+};
 
-    window.myDoughnut.update();
 
-}
+function gerarRosquinhaSolo() {
+    setInterval(function() {
+        var ctx = document.getElementById('rosquinha_solo').getContext('2d');
+        window.myDoughnutSolo = new Chart(ctx, {
+            type: 'doughnut',
+            data: roscaChartDataSolo,
+            options: {
+                animation: false,
+                responsive: true,
+                legend: {
+                    position: 'left',
+                    display: true,
+                },
+                title: {
+                    text: 'Status dos Sensores',
+                    display: false,
+                },
+                animation: {
+                    animateScale: false,
+                    animateRotate: false
+                }
+            }
+        });
+        dadosSolo()
+    }, 4000)
+};
+
+gerarRosquinhaSolo();
+
+
+
+//-------------------------geração de dados aleatórios------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Projeto SIX MIND/Projeto/projeto-site/public/script/setores.js
+++ Projeto SIX MIND/Projeto/projeto-site/public/script/setores.js
@@ -1,9 +1,10 @@
+//---------------------------------- GRAFICO DE BARRAS (atm) ----------------------------------------
 var Dia = new Date().getDate();
 var Mes = new Date().getMonth() + 1;
 var Ano = new Date().getFullYear();
 var dataAno = Dia + "/" + Mes + "/" + Ano;
 var color = Chart.helpers.color;
-var horizontalBarChartData = {
+var horizontalBarChartDataAtm = {
 	labels: ['Setor 1', 'Setor 2', 'Setor 3', 'Setor 4', 'Setor 5', 'Setor 6'],
 	datasets: [{
 		label: 'Umidade no Setor',
@@ -11,60 +12,142 @@ var horizontalBarChartData = {
 		borderColor: '#28a745',
 		borderWidth: 1,
 		data: []
-	}, ]
+	},]
 
 };
 
-function setoresGrafico() {
-	setInterval(function(){
-		var ctx = document.getElementById('setores').getContext('2d');
-	window.myHorizontalBar = new Chart(ctx, {
-		type: 'horizontalBar',
-		data: horizontalBarChartData,
-		options: {
-			// Elements options apply to all of the options unless overridden in a dataset
-			// In this case, we are setting the border of each horizontal bar to be 2px wide
-			elements: {
-				rectangle: {
-					borderWidth: 2,
-				}
-			},
-			responsive: true,
-			legend: {
-				position: 'right',
-			},
-			title: {
-				display: false,
-				text: `Umidade dos Setores ( ${dataAno} )`
-			},
-			legend: {
-				display: false,
-			},
-
-			scales: {
-				xAxes: [{
-					ticks: {
-						max: 100,
-						min: 0
+function setoresGraficoAtm() {
+	setInterval(function () {
+		var ctx = document.getElementById('setores_atm').getContext('2d');
+	
+		window.myHorizontalBarAtm = new Chart(ctx, {
+			type: 'horizontalBar',
+			data: horizontalBarChartDataAtm,
+			
+			options: {
+				// Elements options apply to all of the options unless overridden in a dataset
+				// In this case, we are setting the border of each horizontal bar to be 2px wide
+				elements: {
+					rectangle: {
+						borderWidth: 2,
 					}
-				}]
+				},
+				animation: false,
+				// duration: 0,
+				responsive: true,
+				legend: {
+					position: 'right',
+				},
+				title: {
+					display: false,
+					text: `Umidade dos Setores ( ${dataAno} )`
+				},
+				legend: {
+					display: false,
+				},
+
+				scales: {
+					xAxes: [{
+						ticks: {
+							max: 100,
+							min: 0
+						}
+					}]
+				}
 			}
-		}
-	});
-	gerardados()
-}, 3200)
+		});
+		gerardados_atm()
+	}, 4000)
 };
-		
 
-setoresGrafico()
 
-function gerardados(){
-    horizontalBarChartData.datasets[0].data = [];
-    for(let cont = 0; cont < 7; cont ++){
-        horizontalBarChartData.datasets[0].data.push(parseInt(Math.random() * (39 + 1) + 30))        
+setoresGraficoAtm()
 
-    }
+function gerardados_atm() {
+	horizontalBarChartDataAtm.datasets[0].data = [];
+	for (let cont = 0; cont < 7; cont++) {
+		horizontalBarChartDataAtm.datasets[0].data.push(parseInt(Math.random() * (39 + 1) + 30))
 
-    window.myHorizontalBar.update();
-   
+	}
+
+	// window.myHorizontalBarAtm.destroy();
+	// setoresGraficoAtm();
+	window.myHorizontalBarAtm.update();
+
+}
+
+// ------------------------- GRAFICO DE BARRAS (solo) --------------------------------------
+
+var Dia = new Date().getDate();
+var Mes = new Date().getMonth() + 1;
+var Ano = new Date().getFullYear();
+var dataAno = Dia + "/" + Mes + "/" + Ano;
+var color = Chart.helpers.color;
+var horizontalBarChartDataSolo = {
+	labels: ['Setor 1', 'Setor 2', 'Setor 3', 'Setor 4', 'Setor 5', 'Setor 6'],
+	datasets: [{
+		label: 'Umidade no Setor',
+		backgroundColor: '#007bff',
+		borderColor: '#007bff',
+		borderWidth: 1,
+		data: []
+	},]
+
+};
+
+function setoresGraficoSolo() {
+	setInterval(function () {
+		var ctx = document.getElementById('setores_solo').getContext('2d');
+		window.myHorizontalBarSolo = new Chart(ctx, {
+			type: 'horizontalBar',
+			data: horizontalBarChartDataSolo,
+			options: {
+				// Elements options apply to all of the options unless overridden in a dataset
+				// In this case, we are setting the border of each horizontal bar to be 2px wide
+				elements: {
+					rectangle: {
+						borderWidth: 2,
+					}
+				},
+				animation: false,
+
+				responsive: true,
+				legend: {
+					position: 'right',
+				},
+				title: {
+					display: false,
+					text: `Umidade dos Setores ( ${dataAno} )`
+				},
+				legend: {
+					display: false,
+				},
+
+				scales: {
+					xAxes: [{
+						ticks: {
+							max: 100,
+							min: 0
+						}
+					}]
+				}
+			}
+		});
+		gerardados_solo()
+	}, 4000)
+};
+
+
+setoresGraficoSolo()
+
+function gerardados_solo() {
+	horizontalBarChartDataSolo.datasets[0].data = [];
+	for (let cont = 0; cont < 7; cont++) {
+		horizontalBarChartDataSolo.datasets[0].data.push(parseInt(Math.random() * (39 + 1) + 30))
+
+	}
+
+	window.myHorizontalBarSolo.update();
+	// setoresGraficoSolo()
+
 }
