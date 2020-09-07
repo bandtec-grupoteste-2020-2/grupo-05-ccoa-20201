62f6b99eed8d29bdd2610a0302fdc1b1c125fcfe
CarlosSantanaSP <carlos.santana@bandtec.com.br>
2020-09-07 17:18:34 -0300

    Projeto de aquisição de dados de máquina finalizado

---
+++ Projeto Marise (Python)/aquisicao_de_dados/main.py
@@ -0,0 +1,28 @@
+from services.mysql import Mysql
+from services.dataGenerator import get_data_cpu
+from services.dataGenerator import get_data_ram
+from services.dataGenerator import get_data_disco
+
+import time
+
+#Inserir user, password, host, database
+mysql = Mysql('urubu100','urubu100', 'localhost', 'MAQUINAS')
+
+mysql.connect()
+
+while True:
+    # values = getData()
+    # values_ram = get_data_ram()
+   
+    values_ram = get_data_ram()
+    mysql.insert_ram(values_ram)
+    # time.sleep(5)
+
+    values_cpu = get_data_cpu()
+    mysql.insert_cpu(values_cpu)
+
+    values_disco = get_data_disco()
+    mysql.insert_disco(values_disco)
+    # time.sleep(5)
+    # # mysql.insert(values_cpu)
+    time.sleep(5)
---
+++ Projeto Marise (Python)/aquisicao_de_dados/services/mysql.py
@@ -0,0 +1,90 @@
+import mysql.connector
+
+
+class Mysql:
+    def __init__(self, user, password, host, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
+        self.mysql = None
+        self.cursor = None
+
+    #Estabelecendo uma conexão
+    def connect(self):
+        try:
+            self.mysql = mysql.connector.connect(
+            user=self.user, password=self.password, host=self.host, database=self.database, auth_plugin='mysql_native_password')
+            #Criando cursor para manipulação do banco.
+            print(self.mysql)
+            self.cursor = self.mysql.cursor()
+        except Exception as err:
+            print(err)
+            raise
+
+    def insert_ram(self, data):
+        query = (
+            "INSERT INTO `RamLeituras`(ramPorcentagem, ramUsadaGb, tempoLeitura, fkMaquina)"
+            "VALUES (%s, %s, %s, %s)"    
+        )    
+        values = data
+        try:
+            # print('Inserindo Valores')
+            self.cursor.execute(query,values)
+            self.mysql.commit()
+        except Exception as err:
+            print(err)
+            self.mysql.rollback()
+            self.close()
+
+
+
+    def insert_cpu(self, data):
+
+        for k in range(len(data[0])):
+
+            query = (
+                "INSERT INTO `CPULeituras`(nomeCPU, CPUPorcentagem, tempoLeitura, fkMaquina)"
+                "VALUES (%s, %s, %s, %s)"    
+            )    
+
+            values = ("CPU_{}".format(k), data[0]["CPU_{}".format(k)], data[1], data[2])
+            try:
+                # for cpu in values:
+                # print('Inserindo Valores')
+                self.cursor.execute(query,values)
+                self.mysql.commit()
+            except Exception as err:
+                print(err)
+                self.mysql.rollback()
+                self.close()          
+
+    def insert_disco(self, data):
+
+        for key in data[0]:
+
+            query = (
+                "INSERT INTO `DiscosLeitura`(discoNome, discoPercentual, tempoLeitua, fkMaquina)"
+                "VALUES (%s, %s, %s, %s)"    
+            )    
+            #O data[0] ainda não há certeza, 
+            #mas se leva a crer que esteja correto. Obs.:César
+            
+            values = (key, data[0][key], data[1], data[2])
+            try:
+                # for cpu in values:
+                # print('Inserindo Valores')
+                self.cursor.execute(query,values)
+                self.mysql.commit()
+            except Exception as err:
+                print(err)
+                self.mysql.rollback()
+                self.close()  
+    
+    # Fechando conexão
+    def close(self):
+        self.mysql.close()
+
+        
+
+
