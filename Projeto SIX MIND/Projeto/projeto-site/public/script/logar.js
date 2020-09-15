b92c8e22ada2fabb52f8e2cc2c7aa37f86c2d217
Cesar-Almeida <cesar.almeida@bandtec.com>
2020-09-15 13:12:25 -0300

    Conexão Dash

--- Projeto SIX MIND/Projeto/projeto-site/public/script/logar.js
+++ Projeto SIX MIND/Projeto/projeto-site/public/script/logar.js
@@ -11,7 +11,6 @@ function entrar() {
                 // Modificar
                 sessionStorage.login_usuario_meuapp = json.Email;
                 sessionStorage.senha_usuario_meuapp = json.Senha;
-                
                 window.location.href = 'dashboard.html';
             });
 
