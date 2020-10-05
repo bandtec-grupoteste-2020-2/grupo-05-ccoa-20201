dbdc440f59b74447b9ff6150e6f17b6e3b7f3cc7
marcelo-bonora <marcelo.bonora@bandtec.com.br>
2020-10-04 23:44:50 -0300

    04/10/2020 Pequeno ajuste na tela de cadastro e dashboard

--- Projeto SIX MIND/Projeto/projeto-site/routes/index.js
+++ Projeto SIX MIND/Projeto/projeto-site/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const axios = require("axios");
-let url_bot = 'https://hooks.slack.com/services/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';
+let url_bot = 'https://hooks.slack.com/services/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';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
