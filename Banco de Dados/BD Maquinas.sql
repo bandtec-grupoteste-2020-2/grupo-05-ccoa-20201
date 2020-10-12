caa8144dccef887618a05bd7e2c9a80c2db9f6cb
Aluno <arthur.assis@bandtec.com>
2020-10-12 20:54:09 -0300

    Banco de Dados Completado

--- Banco de Dados/BD Maquinas.sql
+++ Banco de Dados/BD Maquinas.sql
@@ -29,32 +29,57 @@ create table Maquina(
     foreign key(Usuario_idUsuario) references Usuario(id_Usuario)
 );
 
-create table RamLeituras(
-	idLeituraRam int primary key auto_increment,
-    ramPorcentagem varchar(45),
-    ramUsadaGb int,
-    tempoLeitura varchar(45),
-    fkMaquina int,
-    foreign key(fkMaquina) references Maquina(idMaquina)
+create table Componente(
+	idComponente int primary key auto_increment,
+    nomeComponente varchar(45),
+    metrica varchar(45)
 );
 
-create table DiscosLeitura(
-	idLeituraDisco int primary key auto_increment,
-    discoNome varchar(45),
-    discoPercentual varchar(45),
-    tempoLeitura varchar(45),
+create table MaquinaComponente(
+	idMaquinaComponente int primary key auto_increment,
     fkMaquina int,
-    foreign key(fkMaquina) references Maquina(idMaquina)
+    foreign key(fkMaquina) references Maquina(idMaquina),
+    fkComponente int,
+    foreign key(fkComponente) references Componente(idComponente),
+    ativado bit,
+    check (ativado = 0 or ativado = 1)
 );
 
-create table CPULeituras(
-	idLeituraCPU int primary key auto_increment,
-    nomeCPU varchar(45),
-    CPUPorcentagem varchar(45),
-    tempoLeitura varchar(45),
-    fkMaquina int,
-    foreign key(fkMaquina) references Maquina(idMaquina)
+create table Leitura(
+	idLeituras int primary key auto_increment,
+    descricao varchar(45),
+    valorLeitura decimal (7,2),
+    tempoleitura datetime,
+    fkMaquinaComponente int,
+    foreign key(fkMaquinaComponente) references MaquinaComponente(idMaquinaComponente)
 );
+
+insert into MaquinaComponente (idMaquinaComponente, fkMaquina, fkComponente, Ativado) values 
+	(1, 1, 1),
+	(1, 2, 1), 
+    (1, 3, 1),
+    (1, 4, 1),
+    (1, 5, 1),
+    (2, 1, 1),
+    (2, 2, 1), 
+    (2, 3, 1),
+    (2, 4, 1),
+    (2, 5, 1),
+    (3, 1, 1),
+    (3, 2, 1),
+    (3, 3, 1),
+    (3, 4, 1),
+    (3, 5, 1);
+
+insert into Componente (idComponente, nomeComponente) values 
+	('Disco Porcentagem', 'porcentagem'), 
+    ('Disco Gb', 'Gb'),
+    ('Memoria porcentagem', 'porcentagem'),
+    ('Memoria Gb', 'Gb'),
+    ('CPU porcentagem', 'porcentagem'),
+    ('Temperatura ºC', 'ºC'),
+    ('Clock Hz', 'Hz'),
+    ('Rede kbps', 'kbps');
 
 insert into Maquina values
 	(null,"4", "4"),
