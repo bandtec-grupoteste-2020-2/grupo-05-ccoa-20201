7cb50d987a0e9c881fbecdf618eb896a0bdb7590
marcelo-bonora <marcelo.bonora@bandtec.com.br>
2020-10-23 13:45:16 -0200

    23/10/2020 - Coluna media adicionada

--- Projeto Gravar dados(Python)/aquisicao_de_dados/Leituras(CSV)/Script_Core_%.R
+++ Projeto Gravar dados(Python)/aquisicao_de_dados/Leituras(CSV)/Script_Core_%.R
@@ -13,30 +13,31 @@ if (Maquina_IOT >= 1 || Maquina.IOT >= 1) {
 #Criação de cada plot (dias da semana)
 {
   par(mfrow=c(2,2), mar=c(4,4,3.5,0.5))
-  plot(as.Date(cpu_core1_uso_perc$dataleitura),cpu_core1_uso_perc$valor,main = 'Uso da core 1 %/tempo',xlab ="Tempo",ylab = "%" )
-  plot(as.Date(cpu_core2_uso_perc$dataleitura),cpu_core2_uso_perc$valor,main = 'Uso da core 2 %/tempo',xlab ="Tempo",ylab = "%" ) 
-  plot(as.Date(cpu_core3_uso_perc$dataleitura),cpu_core3_uso_perc$valor,main = 'Uso da core 3 %/tempo',xlab ="Tempo",ylab = "%" )
-  plot(as.Date(cpu_core4_uso_perc$dataleitura),cpu_core4_uso_perc$valor,main = 'Uso da core 4 %/tempo',xlab ="Tempo",ylab = "%" )
+  plot(as.Date(cpu_core1_uso_perc$dataleitura),cpu_core1_uso_perc$valor,main = 'Uso da core 1 %/tempo',xlab ="Tempo",ylab = "%" ,ylim=c(0,100))
+  plot(as.Date(cpu_core2_uso_perc$dataleitura),cpu_core2_uso_perc$valor,main = 'Uso da core 2 %/tempo',xlab ="Tempo",ylab = "%" ,ylim=c(0,100)) 
+  plot(as.Date(cpu_core3_uso_perc$dataleitura),cpu_core3_uso_perc$valor,main = 'Uso da core 3 %/tempo',xlab ="Tempo",ylab = "%" ,ylim=c(0,100))
+  plot(as.Date(cpu_core4_uso_perc$dataleitura),cpu_core4_uso_perc$valor,main = 'Uso da core 4 %/tempo',xlab ="Tempo",ylab = "%" ,ylim=c(0,100))
 }
+
 
 #Criação de cada plot (ordenado por Id)
 {
-  plot(Core1_porc$valor~Core1_porc$idLeitura, main = "Core 1 % / id", xlab = "tempo", ylab = "%")
-  plot(Core2_porc$valor~Core2_porc$idLeitura, main = "Core 2 % / id", xlab = "tempo", ylab = "%")
-  plot(Core3_porc$valor~Core3_porc$idLeitura, main = "Core 3 % / id", xlab = "tempo", ylab = "%")
-  plot(Core4_porc$valor~Core4_porc$idLeitura, main = "Core 4 % / id", xlab = "tempo", ylab = "%")
+  plot(cpu_core1_uso_perc$valor~cpu_core1_uso_perc$idLeitura, main = "Core 1 % / id", xlab = "tempo", ylab = "%")
+  plot(cpu_core2_uso_perc$valor~cpu_core2_uso_perc$idLeitura, main = "Core 2 % / id", xlab = "tempo", ylab = "%")
+  plot(cpu_core3_uso_perc$valor~cpu_core3_uso_perc$idLeitura, main = "Core 3 % / id", xlab = "tempo", ylab = "%")
+  plot(cpu_core4_uso_perc$valor~cpu_core4_uso_perc$idLeitura, main = "Core 4 % / id", xlab = "tempo", ylab = "%")
 }
 
 #Criação dos Hist
 {
   par(mfrow=c(2,2), mar=c(2.5,4,3.5,0.5))
-  hist(cpu_core1_uso_perc$valor,main = 'Uso da core 1 em Freq / % ',xlab ='Valor da CPU usada em %',ylab = 'Frequência' )
+  hist(cpu_core1_uso_perc$valor,main = 'Uso da core 1 em Freq / %',xlab ='Valor da CPU usada em %',ylab = 'Frequência' )
   hist(cpu_core2_uso_perc$valor,main = 'Uso da core 2 em Freq / %',xlab ='Valor da CPU usada em %',ylab = 'Frequência' )
   hist(cpu_core3_uso_perc$valor,main = 'Uso da core 3 em Freq / %',xlab ='Valor da CPU usada em %',ylab = 'Frequência' )
   hist(cpu_core4_uso_perc$valor,main = 'Uso da core 4 em Freq / %',xlab ='Valor da CPU usada em %',ylab = 'Frequência' )
   
   #Estado em que o core da CPU se encontra
-  #CPU CORE 1 %
+  #CPU CORE 1 
   if (nrow(cpu_core1_uso_perc[cpu_core1_uso_perc$valor <= 30,]) > floor(nrow(cpu_core1_uso_perc)* 0.5)){
     print("CPU Core1 se encontra em estado BOM!")
     
