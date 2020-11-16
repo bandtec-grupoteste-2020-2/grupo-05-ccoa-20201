0a5ed957e3c0b1d03cf11d5caa54a48a26fed17b
henrique-zigon <61800698+henrique-zigon@users.noreply.github.com>
2020-11-16 20:03:19 -0300

    Separação de linhas por core
    Separação de linhas por core no java, precisa arrumar o eixo x

--- Projeto_aplicacao_JAVA/client-java-sixminds/src/main/java/br/com/sixminds/client/java/sixminds/dashboard/Funcoes.java
+++ Projeto_aplicacao_JAVA/client-java-sixminds/src/main/java/br/com/sixminds/client/java/sixminds/dashboard/Funcoes.java
@@ -24,33 +24,64 @@ public class Funcoes {
                 + "Maquina where idComponente=fkComponente and idMaquina=fkMaquina and idMaquina=1 and ativado=1;",
                 new BeanPropertyRowMapper(Componentes.class));
         for(Componentes compon : listaComp){
-            
             System.out.println(compon.getNomecomponente());
             
-            List<LeituraComponente> listaCada=jdbcTemplate1.query(
-                "select idLeitura,idComponente,nomeComponente,descricao,idMaquinaComponente,ativado,valor from Usuario,Maquina,MaquinaComponente,"
-                + "Componente,Leitura where nomeUsuario=\"João\" and idMaquina=1 and idUsuario=Usuario_idUsuario\n" +
-                " and idMaquina=fkMaquina and nomeComponente=? and idComponente=fkComponente and idMaquinaComponente=fkMaquinaComponente limit 10;",
-                new BeanPropertyRowMapper(LeituraComponente.class),compon.getNomecomponente());
-            for(LeituraComponente leitura : listaCada){
-                separar(plotCPU,plotDisco,plotMem,leitura,ativadoCPU);
-                System.out.println(leitura.getValor());
+            if (compon.getNomecomponente().equals("CPU_uso")) {
+                funcaoCPU(jdbcTemplate1,plotCPU,plotMem,plotDisco,ativadoCPU,compon);
+            }
+            else{
+                List<LeituraComponente> listaCada=jdbcTemplate1.query(
+                    "select idLeitura,idComponente,nomeComponente,descricao,idMaquinaComponente,ativado,valor from Usuario,Maquina,MaquinaComponente,"
+                    + "Componente,Leitura where nomeUsuario=\"João\" and idMaquina=1 and idUsuario=Usuario_idUsuario\n" +
+                    " and idMaquina=fkMaquina and nomeComponente=? and idComponente=fkComponente and idMaquinaComponente=fkMaquinaComponente limit 10;",
+                    new BeanPropertyRowMapper(LeituraComponente.class),compon.getNomecomponente());
+
+                for(LeituraComponente leitura : listaCada){
+                    separar(plotCPU,plotDisco,plotMem,leitura,ativadoCPU);
+                    System.out.println(leitura.getValor());
+                }
             }
         }
     }
+    
+    
+    void funcaoCPU(JdbcTemplate jdbcTemplate1,LineChart plotCPU,LineChart plotMem,LineChart plotDisco,String ativadoCPU,Componentes compon){
+        /*-CONTA QUANTOS NUCLEOS TEM NO PCZAO-*/
+        List<Cores> listaQnt=jdbcTemplate1.query(
+                    " select distinct(descricao) from maquina,componente,maquinacomponente,leitura where "
+                    + "nomeComponente=\"CPU_uso\" and fkComponente=idComponente and descricao like \"Core%\" "
+                    + "and idMaquinaComponente=fkMaquinaComponente and fkMaquina=idMaquina;",new BeanPropertyRowMapper(Cores.class));
+
+        for (Cores core : listaQnt) {
+            List<LeituraComponente> listaCada=jdbcTemplate1.query(
+                    "select idLeitura,idComponente,nomeComponente,descricao,idMaquinaComponente,ativado,valor from Usuario,Maquina,MaquinaComponente,"
+                    + "Componente,Leitura where nomeUsuario=\"João\" and idMaquina=1 and idUsuario=Usuario_idUsuario\n" +
+                    " and idMaquina=fkMaquina and nomeComponente=? and descricao=? and idComponente=fkComponente and idMaquinaComponente=fkMaquinaComponente limit 10;",
+                    new BeanPropertyRowMapper(LeituraComponente.class),compon.getNomecomponente(),core.getDescricao());
+            for(LeituraComponente leitura : listaCada){
+                separar(plotCPU,plotDisco,plotMem,leitura,ativadoCPU);
+                    System.out.println(leitura.getDescricao());
+                    System.out.println(leitura.getValor());
+            }
+        }
+    }
+       
+    
+    
+    
     // Esses JPanel é só pra ler, mas na real são line charts
     void separar(LineChart jPanelCPU,LineChart jPanelMemoria,LineChart jPanelDisco,LeituraComponente leitura,String ativadoCPU){
  
         if (leitura.getNomeComponente().equals("CPU_uso") || leitura.getNomeComponente().equals("CPU_temperatura") || leitura.getNomeComponente().equals("CPU_clock")) {
             if (ativadoCPU.equals("nada")) {
                 if (leitura.getNomeComponente().equals("CPU_uso")) {
-                    jPanelCPU.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
+                    jPanelCPU.inserir(leitura.getValor(),leitura.getDescricao(),leitura.getIdLeitura());
                 }
             }
             else{
                 if (ativadoCPU.equals("CPU_uso(%)")) {
                     if (leitura.getNomeComponente().equals("CPU_uso")) {
-                        jPanelCPU.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
+                        jPanelCPU.inserir(leitura.getValor(),leitura.getDescricao(),leitura.getIdLeitura());
                     }
                 }
                 else if(ativadoCPU.equals("Temperatura")){
@@ -66,15 +97,12 @@ public class Funcoes {
             }   
         }
 
-        
-        
-        
-        else if (leitura.getNomeComponente().equals("Disco_uso")) {;
-            jPanelDisco.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
-        }
-        else if(leitura.getNomeComponente().equals("Memória_uso")){
-            jPanelMemoria.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
-        }
+//        else if (leitura.getNomeComponente().equals("Disco_uso")) {
+//            jPanelDisco.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
+//        }
+//        else if(leitura.getNomeComponente().equals("Memória_uso")){
+//            jPanelMemoria.inserir(leitura.getValor(),leitura.getNomeComponente(),leitura.getIdLeitura());
+//        }
         
     }
 }
