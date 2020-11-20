6a0a766ba470de4f3eba5fbe39a7e5e3c916a666
thiago-yuiti <thiago.tomaru@bandtec.com.br>
2020-11-20 16:44:02 -0300

    Correção do index.js e collapse
    Co-Authored-By: CarlosSantanaSP <61557079+CarlosSantanaSP@users.noreply.github.com>

--- Projeto_SIXMINDS_Web/Projeto/Projeto-Maquina/routes/index.js
+++ Projeto_SIXMINDS_Web/Projeto/Projeto-Maquina/routes/index.js
@@ -9,31 +9,31 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// router.post('/slack', function (req, res, next) {
-//   console.log(`Enviando menssagem slack`);
-//   console.log("ola senhores");
+router.post('/slack', function (req, res, next) {
+  console.log(`Enviando menssagem slack`);
+  console.log("ola senhores");
 
-//   var subject = req.body.input_subject;
-//   var email = req.body.input_email;
-//   var description = req.body.textarea_description;
+  var subject = req.body.input_subject;
+  var email = req.body.input_email;
+  var description = req.body.textarea_description;
 
-//   var message = `${subject} \n\n ${description} \n\n Enviado por: ${email}`;
-//   send_message_slack(message);
+  var message = `${subject} \n\n ${description} \n\n Enviado por: ${email}`;
+  send_message_slack(message);
 
-//   res.redirect("/dashboard.html")
-//   // res.location("http://pt.stackoverflow.com")
-//   // res.json()
+  res.redirect("/dashboard.html")
+  // res.location("http://pt.stackoverflow.com")
+  // res.json()
 
-// })
+})
 
-// function send_message_slack(message) {
-//   axios.post(
-//     url_bot,
-//     { text: message }
-//   )
+function send_message_slack(message) {
+  axios.post(
+    url_bot,
+    { text: message }
+  )
 
 
-// }
+}
 
 
 module.exports = router;
